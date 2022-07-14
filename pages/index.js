e2fcc96850e1c3236e2173c29e5bdb8d337c505e
Phong Mai <phongspacenasa@gmail.com>
2022-07-14 11:12:30 +0700

    done fetching API from the movie databse

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Container, Result } from 'postcss'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+
+export default function Home(props) {
+  console.log(props);
   return (
     <div>
       <Head>
@@ -19,6 +24,23 @@ export default function Home() {
       <Nav />
 
       {/* Container */}
+      <Results />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
